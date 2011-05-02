891a3c183380b56f094b3fd89aa3997194912ac1
ItzWarty <ItzWarty@b1933033-5620-0bc0-2ddb-ace7f0520832>
2011-05-02 20:23:49 +0000

    Code cleaning... Some housekeeping's being done

--- RAF_Browser/Program.cs
+++ RAF_Browser/Program.cs
@@ -23,18 +23,6 @@ namespace RAFLib
         static void Main()
         {
 #if PACK
-            Environment.CurrentDirectory = Environment.CurrentDirectory + "\\";
-            //Environment.CurrentDirectory = @"C:\Riot Games\League of Legends\RADS\projects\lol_game_client\filearchives\0.0.0.29\";
-            Console.WriteLine("Experimental RAF Packer by ItzWarty @ ItzWarty.com April 29 2011 3:37pm pst");
-            Console.WriteLine("Only use-able after RAF Dumper, for now.");
-            //Pack whatever dump folder is sitting next to us, write to pack folder
-            if(Directory.Exists(Environment.CurrentDirectory+@"\dump\"))
-                new RAFPacker().PackRAF(
-                    Environment.CurrentDirectory+@"\dump\", 
-                    Environment.CurrentDirectory+@"\pack\"
-                );
-            else
-                MessageBox.Show("Place RAFDUMP executable next to 'dump' folder.  Upon run, the 'pack' folder will be created w/ raf+raf.dat");
             return;
 #endif
             Console.WriteLine("Experimental RAF Dumper by ItzWarty @ ItzWarty.com April 28 2011 10:00pm pst");
---
+++ RAF_Browser/RAFManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+using RAFLib.Util;
+
+using System.Windows.Forms;
+
+namespace RAFLib
+{
+    public class RAFManager
+    {
+        public static bool Pack(string directory)
+        {
+            //Environment.CurrentDirectory = directory;
+            //Environment.CurrentDirectory = @"C:\Riot Games\League of Legends\RADS\projects\lol_game_client\filearchives\0.0.0.29\";
+            Console.WriteLine("Experimental RAF Packer by ItzWarty @ ItzWarty.com April 29 2011 3:37pm pst");
+            Console.WriteLine("Only use-able after RAF Dumper, for now.");
+            //Pack whatever dump folder is sitting next to us, write to pack folder
+            if (Directory.Exists(Environment.CurrentDirectory + @"\dump\"))
+            {
+                return new RAFPacker().PackRAF(
+                    Environment.CurrentDirectory + @"\dump\",
+                    Environment.CurrentDirectory + @"\pack\"
+                );
+            }
+            else
+                return false;
+            //else
+            //    MessageBox.Show("Place RAFDUMP executable next to 'dump' folder.  Upon run, the 'pack' folder will be created w/ raf+raf.dat");
+        }
+    }
+}
