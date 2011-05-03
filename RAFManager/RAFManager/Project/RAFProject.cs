9bb3ac3914d27d902af57acdde405f881deefa9e
ItzWarty <ItzWarty@b1933033-5620-0bc0-2ddb-ace7f0520832>
2011-05-03 21:16:08 +0000

    Projects development.

---
+++ RAFManager/RAFManager/Project/RAFProject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RAFManager.Project
+{
+    public class RAFProject
+    {
+        RAFProjectInfo projectInfo  = null;
+        public bool hasChanged      = false;
+        public RAFProject()
+        {
+            projectInfo = new RAFProjectInfo();
+            projectInfo.ProjectName = "Untitled Project";
+            projectInfo.ProjectPath = "";
+            projectInfo.FileArchivesDirectory = "";
+        }
+        public RAFProjectInfo ProjectInfo
+        {
+            get
+            {
+                return projectInfo;
+            }
+        }
+        public bool HasChanged
+        {
+            get
+            {
+                return hasChanged;
+            }
+        }
+
+        public void Save(string location)
+        {
+            string serialization = projectInfo.ProjectName;
+        }
+    }
+}
