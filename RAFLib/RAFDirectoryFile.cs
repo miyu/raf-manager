2952d04b1a608c0b4f8b6d80f23b988426e4d361
ItzWarty <ItzWarty@b1933033-5620-0bc0-2ddb-ace7f0520832>
2011-05-10 07:57:35 +0000

    Descriptive rightclick context menus, rename node, delete node, search method updated

--- RAFLib/RAFDirectoryFile.cs
+++ RAFLib/RAFDirectoryFile.cs
@@ -40,8 +40,10 @@ namespace RAFLib
         byte[] content = null;
         RAFFileList fileList = null;
         RAFStringTable stringTable = null;
+        RAFArchive raf = null;
         public RAFDirectoryFile(RAFArchive raf, string location)
         {
+            this.raf = raf;
             content = System.IO.File.ReadAllBytes(location);
             magic = BitConverter.ToUInt32(content.SubArray(0, 4), 0);
             version = BitConverter.ToUInt32(content.SubArray(4, 4), 0);
@@ -81,11 +83,103 @@ namespace RAFLib
         /// <summary>
         /// Returns the DirectoryFile that is currently in memory.
         /// If it fileEntries have been changed, this will reflect those changes.
+        /// 
+        /// NOTE THAT THIS ONLY RETURNS CORRECT VALUES IF YOU HAVENT' CHANGED THE RAF FILE
         /// </summary>
         /// <returns></returns>
+        [Obsolete("This method has been superceded by RAFDirectoryFile.GetBytes().  If you decide to call this, you will obtain "+
+                  "an unmodified RAF Directory File.")]
         public byte[] GetContent()
         {
+            Console.WriteLine(
+                "NOTICE: GETCONTENT() IS DEPRECATED.  IT WILL ONLY BE CORRECT\r\n" +
+                "IF THE CONTENT OF THE DIRECTORY FILE & RAF ARCHIVE DAT STAYS STATIC"
+            );
             return content;
+        }
+        public byte[] GetBytes()
+        {
+            //Calls to bitconverter were avoided until the end... just to make code prettier
+
+            List<RAFFileListEntry> fileListEntries = fileList.GetFileEntries();
+
+            List<UInt32> result = new List<UInt32>();
+            //Header
+            result.Add(magic);
+            result.Add(version);
+
+            //Table of Contents
+            result.Add(mgrIndex);
+            result.Add(5 * 4);  //Offset of file list
+            result.Add(
+                (UInt32)(
+                       5 * 4 + 4 + /*file list offset and entry itself*/
+                       4 * 4 * fileListEntries.Count /* Size of all entries total */
+                )//Offset to string table
+            );
+
+            //File List Header
+            result.Add((UInt32)fileListEntries.Count); //F
+
+            {   //File List Entries
+                UInt32 i = 0;
+                foreach(RAFFileListEntry entry in fileListEntries)
+                {
+                    result.Add(entry.StringNameHash);
+                    result.Add(entry.FileOffset);
+                    result.Add(entry.FileSize);
+                    result.Add(i++);
+                }
+            }
+
+            //String table Header.
+            int stringTableHeader_SizeOffset = result.Count; //We will store this value later...
+            result.Add(1337); //This value will be changed later to reflect the size of the string table
+            result.Add((UInt32)fileListEntries.Count);  //# strings in table
+
+            //UInt32[] offsets = new UInt32[fileListEntries.Count]; //Stores offsets for entries
+            
+            //Set currentOffset to point to where our strings will be stored
+            UInt32 currentOffset = 4 * 2 /*StringTableHeader Size*/ + (UInt32)(4 * 2 * fileListEntries.Count);
+
+            List<byte> stringTableContent = new List<byte>();
+            {   //Insert entry, add filename to our string name bytes
+                UInt32 i = 0;
+                foreach(RAFFileListEntry entry in fileListEntries)
+                {
+                    result.Add(currentOffset); //offset to this string
+                    result.Add((UInt32)entry.FileName.Length + 1);
+                    currentOffset += (UInt32)entry.FileName.Length + 1;
+                    stringTableContent.AddRange(
+                        Encoding.ASCII.GetBytes(entry.FileName)
+                    );
+                    stringTableContent.Add(0);
+                    i++;
+                }
+            }
+            //Update string table header with size of all data
+            result[stringTableHeader_SizeOffset] = currentOffset;
+
+            byte[] resultOutput = new byte[result.Count * 4 + stringTableContent.Count];
+            for (int i = 0; i < result.Count; i++)
+            {
+                Array.Copy(
+                    BitConverter.GetBytes(result[i]), 0, resultOutput, i * 4, 4
+                );
+            }
+            Array.Copy(stringTableContent.ToArray(), 0, resultOutput, result.Count * 4, stringTableContent.Count);
+            return resultOutput;
+        }
+
+        public RAFFileListEntry CreateFileEntry(string rafPath, UInt32 offset, UInt32 fileSize, UInt32 nameStringTableIndex)
+        {
+            RAFFileListEntry result = new RAFFileListEntry(this.raf, rafPath, offset, fileSize, nameStringTableIndex);
+            this.GetFileList().AddFileEntry(result);
+            return result;
+        }
+        public void DeleteFileEntry(RAFFileListEntry entry)
+        {
+            this.GetFileList().DeleteFileEntry(entry);
         }
         /*
         private void PrintFileList()
